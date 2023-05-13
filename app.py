d6ec183caa8b88880c4686c9b9882f77fc4b04c1
Tushar Agarwal <tushar.agarwal0901@gmail.com>
2023-05-13 12:55:06 +0530

    Stable Build
    Perfected login, signup features. Added flexibility to add multiple email addresses. Updated UI

--- app.py
+++ app.py
@@ -4,13 +4,28 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import json
 import re
 import time
+import os
 
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'filesystem'
 app.config['SECRET_KEY'] = 'your_secret_key'
 Session(app)
 
-users = []
+DATA_FILE = 'data.json'
+
+def load_data():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, 'r') as f:
+            return json.load(f)
+    return {'users': [], 'email_timers': {}}
+
+def save_data(data):
+    with open(DATA_FILE, 'w') as f:
+        json.dump(data, f)
+
+data = load_data()
+users = data['users']
+email_timers = data['email_timers']
 
 def email_exists(email_timers, email):
     return any(timer['email'] == email for timer in email_timers)
@@ -28,13 +43,15 @@ def signup():
         password = request.form['password']
 
         if get_user_by_email(email) is not None:
-            flash('Email already exists. Please log in or use another email.', 'error')
+            flash('Email already exists..Please log in or use another email..', 'error')
             return redirect(url_for('signup'))
 
         hashed_password = generate_password_hash(password)
         new_user = {'email': email, 'password': hashed_password}
         users.append(new_user)
-        flash('Account created successfully. Please log in.', 'success')
+        data['users'] = users
+        save_data(data)
+        flash('Account created successfully..Please log in..', 'success')
         return redirect(url_for('login'))
 
     return render_template('signup.html')
@@ -47,11 +64,11 @@ def login():
         user = get_user_by_email(email)
 
         if user is None or not check_password_hash(user['password'], password):
-            flash('Invalid email or password. Please try again.', 'error')
+            flash('Invalid email or password..Please try again..', 'error')
             return redirect(url_for('login'))
 
         session['user_email'] = email
-        flash('Logged in successfully.', 'success')
+        flash('Logged in successfully..', 'success')
         return redirect(url_for('index'))
 
     return render_template('login.html')
@@ -59,37 +76,45 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('user_email', None)
-    flash('Logged out successfully.', 'success')
+    flash('Logged out successfully..', 'success')
     return redirect(url_for('login'))
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if 'user_email' not in session:
         return redirect(url_for('login'))
 
-    email_timers = session.get('email_timers', [])
+    user_email = session['user_email']
+    user_email_timers = email_timers.get(user_email, [])
 
     if request.method == 'POST':
         email_input = request.form['email']
-        emails = re.split(r'[,;\s]\s*', email_input.strip())
+
+        emails = re.split(r'[,; \s]\s*', email_input.strip())
 
         for email in emails:
-            if not email_exists(email_timers, email):
-                email_timers.append({'email': email, 'status': 'Ready', 'end_time': 0})
+            if not email_exists(user_email_timers, email):
+                user_email_timers.append({'email': email, 'status': 'Ready', 'end_time': 0})
 
-        session['email_timers'] = email_timers
+        email_timers[user_email] = user_email_timers
+        data['email_timers'] = email_timers
+        save_data(data)
 
-    return render_template('index.html', email_timers=email_timers)
+    return render_template('index.html', email_timers=user_email_timers)
 
 @app.route('/start_timer/<int:email_timer_index>')
 def start_timer(email_timer_index):
     if 'user_email' not in session:
         return redirect(url_for('login'))
 
-    email_timers = session['email_timers']
-    email_timer = email_timers[email_timer_index]
+    user_email = session['user_email']
+    user_email_timers = email_timers[user_email]
+    email_timer = user_email_timers[email_timer_index]
     email_timer['status'] = 'InUse'
-    email_timer['end_time'] = int(time.time()) + 60
-    session['email_timers'] = email_timers
+    email_timer['end_time'] = int(time.time()) + 11700
+    email_timers[user_email] = user_email_timers
+    data['email_timers'] = email_timers
+    save_data(data)
 
     return redirect(url_for('index'))
 
@@ -98,9 +123,12 @@ def delete_timer(email_timer_index):
     if 'user_email' not in session:
         return redirect(url_for('login'))
 
-    email_timers = session['email_timers']
-    del email_timers[email_timer_index]
-    session['email_timers'] = email_timers
+    user_email = session['user_email']
+    user_email_timers = email_timers[user_email]
+    del user_email_timers[email_timer_index]
+    email_timers[user_email] = user_email_timers
+    data['email_timers'] = email_timers
+    save_data(data)
 
     return redirect(url_for('index'))
 
